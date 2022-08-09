ececa88780e3af88e6e90089b7bffde8ad3a2bb6
넵플러스 <kmc901206@gmail.com>
2022-08-09 15:17:19 +0900

    BaseFragment에 생명주기 고려하여 mContext 작성

--- app/src/main/java/com/neppplus/storeorderpractice_recyclerview_20220809/fragments/BaseFragment.kt
+++ app/src/main/java/com/neppplus/storeorderpractice_recyclerview_20220809/fragments/BaseFragment.kt
@@ -1,10 +1,18 @@
 package com.neppplus.storeorderpractice_recyclerview_20220809.fragments
 
+import android.content.Context
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 abstract class BaseFragment : Fragment() {
 
-    val mContext = requireContext()
+    lateinit var mContext : Context
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mContext = requireContext()
+    }
 
     abstract fun setupEvents()
     abstract fun setValues()
