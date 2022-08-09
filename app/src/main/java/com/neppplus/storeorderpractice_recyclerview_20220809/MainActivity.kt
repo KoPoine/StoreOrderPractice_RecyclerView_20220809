c6718b4a63e7be12a584d55f0282381e28aa0a8d
넵플러스 <kmc901206@gmail.com>
2022-08-09 15:13:38 +0900

    뷰페이저 / 탭레이아웃 제작 완료

--- app/src/main/java/com/neppplus/storeorderpractice_recyclerview_20220809/MainActivity.kt
+++ app/src/main/java/com/neppplus/storeorderpractice_recyclerview_20220809/MainActivity.kt
@@ -3,11 +3,13 @@ package com.neppplus.storeorderpractice_recyclerview_20220809
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.neppplus.storeorderpractice_recyclerview_20220809.adapters.MainViewPagerAdapter
 import com.neppplus.storeorderpractice_recyclerview_20220809.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding : ActivityMainBinding
+    lateinit var mPagerAdapter : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        mPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        binding.mainViewPager.adapter = mPagerAdapter
 
+        binding.mainTabLayout.setupWithViewPager(binding.mainViewPager)
     }
 }
