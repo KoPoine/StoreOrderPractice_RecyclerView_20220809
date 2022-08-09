61f02490f447c2dcfa9c07e8057667153272d1b8
넵플러스 <kmc901206@gmail.com>
2022-08-09 15:38:32 +0900

    RecyclerView 데이터 표현용 클래스 제작

---
+++ app/src/main/java/com/neppplus/storeorderpractice_recyclerview_20220809/datas/StoreData.kt
@@ -0,0 +1,9 @@
+package com.neppplus.storeorderpractice_recyclerview_20220809.datas
+
+class StoreData(
+    val title : String,
+    val score : Double,
+    val imageUri : String,
+    val phoneNum : String
+) {
+}
